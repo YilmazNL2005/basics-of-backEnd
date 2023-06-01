6d95531c06d70495a7d8ae1249900598475ed8f3
YilmazNL2005 <99069863@mydavinci.nl>
2023-06-01 10:29:00 +0200

    pause

---
+++ index.php
@@ -0,0 +1,22 @@
+    <!DOCTYPE html>	
+    <head>
+
+    </head>
+    <body>
+        <?php
+        echo 'test'
+        ?>
+    </body>
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>Test</title>
+    </head>
+
+    <body>
+        <p>uwu?</p>
+        <?php echo 'uwu' ?>
+    </body>
+</html>
---
+++ time.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>Tijd_opdracht</title>
+        <link rel="stylesheet" href="styleTime.css">
+    </head>
+<body class="morning">
+
+<h1></h1>
+
+
+<?php
+date_default_timezone_set("Europe/Amsterdam");
+echo "The time is " . date("h:i:sa");
+
+?>
+
+</body>
+</html>
