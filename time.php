362cab0184ad53f7c4a7712998f3898193787eef
YilmazNL2005 <99069863@mydavinci.nl>
2023-06-20 10:19:24 +0200

    vink

--- time.php
+++ time.php
@@ -1,20 +1,45 @@
+<?php
+date_default_timezone_set("Europe/Amsterdam");
+$time = date("h:i:sa");
+$hour = date("h");
+// echo "The time is " . date("h:i:sa");
+
+if($hour < 6){
+    $message = "goede nacht";
+    $image = "night";
+}
+elseif ($hour < 12){
+    $message = "goede morgen";
+    $image = "morning";
+}
+elseif($hour < 18){
+    $message = "goede middag";
+    $image = "evening";
+}
+elseif($hour <= 23){
+    $message = "goede avond";
+    $image = "afternoon";
+}
+?>
+    <div>
+    <?php
+        echo $message;
+        echo "<br>";
+        echo "Het is " . $time;
+    ?>
+    </div>
+
+
 <!DOCTYPE html>
 
 <html lang="en">
     <head>
-        <title>Tijd_opdracht</title>
+        <meta charset="UTF-8">
+        <title> <?php echo $message ?></title>
         <link rel="stylesheet" href="styleTime.css">
     </head>
-<body class="morning">
+<body class= "<?php echo $image ?>">
 
-<h1></h1>
-
-
-<?php
-date_default_timezone_set("Europe/Amsterdam");
-echo "The time is " . date("h:i:sa");
-
-?>
 
 </body>
 </html>
