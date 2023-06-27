3dd7d1099c6e3e61342fa7b1c174694d2f37a691
YilmazNL2005 <99069863@mydavinci.nl>
2023-06-27 12:03:22 +0200

    end

---
+++ welcome.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>welkom</title>
+        <!-- <title> <?php echo $message ?></title> -->
+        <!-- <link rel="stylesheet" href="styleTime.css"> -->
+    </head>
+
+<body>
+    <!-- <body class= "<?php echo $image ?>"> -->
+
+
+
+</body>
+
+</html>
